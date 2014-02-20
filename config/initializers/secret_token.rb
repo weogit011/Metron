a30d2518ec3c880b5fc8b7d4ba5548949ff58454
Antoni Borys <adam.borys515@gmail.com>
2014-02-20 16:40:28 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Metronic::Application.config.secret_key_base = '1daee2b89788909bc61778e21286e1fb50c8a498eae2c89bc1eb3d1cc4db170b38fc3a99ecca1560818e881b4f2a3a450de7aaa8e46281b8d212050bc88c3a38'
